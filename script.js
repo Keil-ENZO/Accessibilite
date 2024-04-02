57b0d5c69586062ea6706ce34a8665b7d0968fa5
Keil-ENZO <enzo.keil06@icloud.com>
2024-04-02 11:27:22 +0200

    produit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function toggleNav() {
 }
 
 // Expressions régulières
-const nameRegex = /^[A-Za-z]+$/;
+const nameRegex = /^[A-Za-z]+(?:[ -][A-Za-z]+)*$/;
 const phoneRegex = /(\+|00)?(33|0)[1-9](\d{2}){4}/;
 const emailRegex = /\S+@\S+\.\S+/;
 
@@ -34,8 +34,11 @@ function validateForm() {
   // Validation du nom et du prénom
   const isNameValid = nameRegex.test(nom) && nameRegex.test(prenom);
   if (!isNameValid) {
-    nomInput.setCustomValidity("Le nom et le prénom doivent contenir uniquement des lettres.");
-    document.getElementById("nomError").textContent = "Le nom et le prénom doivent contenir uniquement des lettres.";
+    nomInput.setCustomValidity(
+      "Le nom et le prénom doivent contenir uniquement des lettres."
+    );
+    document.getElementById("nomError").textContent =
+      "Le nom et le prénom doivent contenir uniquement des lettres.";
   } else {
     nomInput.setCustomValidity("");
     document.getElementById("nomError").textContent = "";
@@ -45,7 +48,8 @@ function validateForm() {
   const isEmailValid = emailRegex.test(email);
   if (!isEmailValid) {
     emailInput.setCustomValidity("Veuillez saisir une adresse email valide.");
-    document.getElementById("emailError").textContent = "Veuillez saisir une adresse email valide.";
+    document.getElementById("emailError").textContent =
+      "Veuillez saisir une adresse email valide.";
   } else {
     emailInput.setCustomValidity("");
     document.getElementById("emailError").textContent = "";
@@ -54,8 +58,11 @@ function validateForm() {
   // Validation du numéro de téléphone
   const isPhoneValid = phoneRegex.test(tel);
   if (!isPhoneValid) {
-    telInput.setCustomValidity("Veuillez saisir un numéro de téléphone valide.");
-    document.getElementById("telError").textContent = "Veuillez saisir un numéro de téléphone valide.";
+    telInput.setCustomValidity(
+      "Veuillez saisir un numéro de téléphone valide."
+    );
+    document.getElementById("telError").textContent =
+      "Veuillez saisir un numéro de téléphone valide.";
   } else {
     telInput.setCustomValidity("");
     document.getElementById("telError").textContent = "";
@@ -64,8 +71,11 @@ function validateForm() {
   // Validation du message
   const isMessageValid = message.length >= 30;
   if (!isMessageValid) {
-    messageInput.setCustomValidity("Le message doit contenir au moins 30 caractères.");
-    document.getElementById("messageError").textContent = "Le message doit contenir au moins 30 caractères.";
+    messageInput.setCustomValidity(
+      "Le message doit contenir au moins 30 caractères."
+    );
+    document.getElementById("messageError").textContent =
+      "Le message doit contenir au moins 30 caractères.";
   } else {
     messageInput.setCustomValidity("");
     document.getElementById("messageError").textContent = "";
@@ -79,13 +89,16 @@ function validateForm() {
   }
 }
 
-
-// Écouter les changements dans le formulaire pour valider en temps réel
-form.addEventListener("input", validateForm);
-
 // Valider le formulaire lors de la soumission
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("Formulaire envoyé !");
-  // Ici, vous pouvez ajouter la logique d'envoi du formulaire si nécessaire
+  validateForm(); // Appeler la fonction de validation ici
+  form.addEventListener("input", validateForm);
+
+  if (form.checkValidity()) {
+    alert("Formulaire envoyé !");
+    form.reset();
+    window.location.reload();
+  }
 });
+
